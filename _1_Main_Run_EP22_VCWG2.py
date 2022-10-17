6e7a5a3506b54a5839d138e6a16bb70c8a0996da
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2022-10-17 13:30:41 -0600

    redone BUBBLE Ue1 only-vcwg

--- _1_Main_Run_EP22_VCWG2.py
+++ _1_Main_Run_EP22_VCWG2.py
@@ -8,27 +8,28 @@ from _3_post_process_code import _0_all_plot_tools as plot_tools
 def run_vcwg():
     # # BUBBLE, CAPITOUL, Vancouver
     # epwFileName = 'Mondouzil_tdb_td_rh_P_2004.epw'
-    # TopForcingFileName = None
-    # # VCWGParamFileName = '_case6_initialize_Basel_BSPA_MOST.uwg'
+    epwFileName = 'Basel.epw'
+    TopForcingFileName = None
+    # VCWGParamFileName = '_case6_initialize_Basel_BSPA_MOST.uwg'
     # VCWGParamFileName = '_case8_initialize_CAPITOUL_MOST.uwg'
-    # # VCWGParamFileName = '_case5_initialize_Basel_BSPR_MOST.uwg'
-    # # ViewFactorFileName = '_case6_BSPA_ViewFactor_Basel_MOST.txt'
+    VCWGParamFileName = '_case5_initialize_Basel_BSPR_MOST.uwg'
+    # ViewFactorFileName = '_case6_BSPA_ViewFactor_Basel_MOST.txt'
     # ViewFactorFileName = '_case8_CAPITOUL_ViewFactor_MOST.txt'
-    # # ViewFactorFileName = '_case5_BSPR_ViewFactor_Basel_MOST.txt'
-    # # Case name to append output file names with
-    # # case = '_BSPA_Refinement_M3ing_Basel_MOST'
-    # case = '_CAPITOUL_MOST'
-    # # case = '_BSPR_Only_VCWG_Basel_MOST'
-
-    # Vancouver
-    # '''
-    epwFileName = None
-    TopForcingFileName = 'Vancouver2008_ERA5_Jul.csv'
-    VCWGParamFileName = '_case7_initialize_Vancouver_LCZ1.uwg'
-    ViewFactorFileName = '_case7_ViewFactor_Vancouver_LCZ1.txt'
+    ViewFactorFileName = '_case5_BSPR_ViewFactor_Basel_MOST.txt'
     # Case name to append output file names with
-    case = '_case7_VancouverTopForcingOnlyVCWG_2008Jul_LCZ1'
-    # '''
+    # case = '_BSPA_Refinement_M3ing_Basel_MOST'
+    # case = '_CAPITOUL_MOST'
+    case = '_BSPR_Only_VCWG_Basel_MOST'
+
+    # # Vancouver
+    # # '''
+    # epwFileName = None
+    # TopForcingFileName = 'Vancouver2008_ERA5_Jul.csv'
+    # VCWGParamFileName = '_case7_initialize_Vancouver_LCZ1.uwg'
+    # ViewFactorFileName = '_case7_ViewFactor_Vancouver_LCZ1.txt'
+    # # Case name to append output file names with
+    # case = '_case7_VancouverTopForcingOnlyVCWG_2008Jul_LCZ1'
+    # # '''
 
     # Initialize the UWG object and run the simulation
     VCWG = VCWG_Hydro(epwFileName, TopForcingFileName, VCWGParamFileName, ViewFactorFileName, case)
@@ -39,12 +40,15 @@ if __name__ == '__main__':
     # Lichen: init the synchronization lock related settings: locks, shared variables.
     coordination.init_saving_data()
     # case_name = 'CAPITOUL_2004_only_vcwg'
-    case_name = 'Vancouver_TopForcing_only_vcwg_2008_Jul'
+    # case_name = 'Vancouver_TopForcing_only_vcwg_2008_Jul'
+    case_name = 'BUBBLE_Ue1_Rural_only_vcwg_2002_June'
     # saving_data_path = '_2_cases_input_outputs\\_07_Vancouver\\vcwg_saving'
     # saving_data_path = '_2_cases_input_outputs\\_08_CAPITOUL\\DOE_Ref_MediumOffice_4B\\vcwg_saving'
-    saving_data_path = '_2_cases_input_outputs\\_07_Vancouver\\TopForcing_Refined_SMALL_OFFICE\\vcwg_saving'
+    # saving_data_path = '_2_cases_input_outputs\\_07_Vancouver\\TopForcing_Refined_SMALL_OFFICE\\vcwg_saving'
+    saving_data_path = '_2_cases_input_outputs\\_05_Basel_BSPR_ue1\\MidRiseApartment_4C_Rural\\vcwg_saving'
     # start_time = '2004-06-01 00:00:00'
-    start_time = '2008-07-01 00:00:00'
+    # start_time = '2008-07-01 00:00:00'
+    start_time = '2002-06-10 00:00:00'
     time_interval_sec = 300
 
     vcwg_thread = Thread(target=run_vcwg)
