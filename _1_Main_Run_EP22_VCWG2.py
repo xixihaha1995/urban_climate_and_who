26804e217c6ec4eb6997a6e28ef2e7440c262ea1
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2022-10-17 13:10:24 -0600

    BUBBLE Ue1 30 done

--- _1_Main_Run_EP22_VCWG2.py
+++ _1_Main_Run_EP22_VCWG2.py
@@ -7,7 +7,8 @@ from _3_post_process_code import _0_all_plot_tools as plot_tools
 def run_ep_api():
     state = api.state_manager.new_state()
     # api.runtime.callback_end_system_timestep_after_hvac_reporting(state, time_step_handlers.mediumOffice_nested_ep_only)
-    api.runtime.callback_end_system_timestep_after_hvac_reporting(state, time_step_handlers.smallOffice_nested_ep_only)
+    # api.runtime.callback_end_system_timestep_after_hvac_reporting(state, time_step_handlers.smallOffice_nested_ep_only)
+    api.runtime.callback_end_system_timestep_after_hvac_reporting(state, time_step_handlers.midRiseApart_nested_ep_only)
     api.exchange.request_variable(state, "HVAC System Total Heat Rejection Energy", "SIMHVAC")
     api.exchange.request_variable(state, "Site Wind Speed", "ENVIRONMENT")
     api.exchange.request_variable(state, "Site Wind Direction", "ENVIRONMENT")
@@ -22,20 +23,24 @@ def run_ep_api():
 if __name__ == '__main__':
     # ep_files_path = '_2_cases_input_outputs\\_06_Basel_BSPA_ue2\\refining_M3ing'
     # case_name = 'CAPITOUL_only_ep_2004'
-    case_name = 'Vancouver_TopForcing_only_ep_2008_July'
+    # case_name = 'Vancouver_TopForcing_only_ep_2008_July'
+    case_name = 'BUBBLE_Ue1_only_ep_2002_June'
     # start_time = '2004-06-01 00:00:00'
-    start_time = '2008-07-01 00:00:00'
+    start_time = '2002-06-10 00:00:00'
     time_interval_sec = 300
     data_name_lst = ['ep_wsp_mps_wdir_deg', 'debugging_canyon']
 
     # ep_files_path = '_2_cases_input_outputs\\_08_CAPITOUL\\DOE_Ref_MediumOffice_4B'
-    ep_files_path = '_2_cases_input_outputs\\_07_vancouver\\TopForcing_Refined_SMALL_OFFICE'
+    # ep_files_path = '_2_cases_input_outputs\\_07_vancouver\\TopForcing_Refined_SMALL_OFFICE'
+    ep_files_path = '_2_cases_input_outputs\\_05_Basel_BSPR_ue1\\MidRiseApartment_4C_Rural'
     data_saving_path = f'{ep_files_path}\\ep_saving'
     # epwFileName = 'Mondouzil_tdb_td_rh_P_2004.epw'
-    epwFileName = 'VancouverTopForcing.epw'
+    # epwFileName = 'VancouverTopForcing.epw'
+    epwFileName = 'Basel.epw'
     # idfFileName = 'RefBldgMidriseApartmentPost1980_v1.4_7.2_4C_USA_WA_SEATTLE-M3ing.idf'
     # idfFileName = 'RefBldgMediumOfficePost1980_v1.4_7.2_4B_USA_NM_ALBUQUERQUE.idf'
-    idfFileName = 'Vancouver_SmallOffice.idf'
+    # idfFileName = 'Vancouver_SmallOffice.idf'
+    idfFileName = 'BUBBLE_Ue1.idf'
     coordination.init_saving_data()
     coordination.init_ep_api()
     api = coordination.ep_api
