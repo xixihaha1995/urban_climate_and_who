362423e86b25f3d19a92935ec55a75a6fa8d225c
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2022-10-19 20:30:37 -0600

    to update the Ue1 only-vcwg data

--- _1_Main_Run_EP22_VCWG2.py
+++ _1_Main_Run_EP22_VCWG2.py
@@ -8,23 +8,24 @@ from _3_post_process_code import _0_all_plot_tools as plot_tools
 def run_vcwg():
     # # BUBBLE, CAPITOUL, Vancouver
     # epwFileName = 'Mondouzil_tdb_td_rh_P_2004.epw'
-    # epwFileName = 'Basel.epw'
+    epwFileName = 'Basel.epw'
     # epwFileName = 'VancouverRural718920.epw'
-    epwFileName = 'Vancouver718920CorrectTime.epw'
+    # epwFileName = 'Vancouver718920CorrectTime.epw'
     TopForcingFileName = None
     # VCWGParamFileName = '_case6_initialize_Basel_BSPA_MOST.uwg'
     # VCWGParamFileName = '_case8_initialize_CAPITOUL_MOST.uwg'
-    # VCWGParamFileName = '_case5_initialize_Basel_BSPR_MOST.uwg'
-    VCWGParamFileName = '_case7_initialize_Vancouver_withRural.uwg'
+    VCWGParamFileName = '_case5_initialize_Basel_BSPR_MOST.uwg'
+    # VCWGParamFileName = '_case7_initialize_Vancouver_withRural.uwg'
     # ViewFactorFileName = '_case6_BSPA_ViewFactor_Basel_MOST.txt'
     # ViewFactorFileName = '_case8_CAPITOUL_ViewFactor_MOST.txt'
-    # ViewFactorFileName = '_case5_BSPR_ViewFactor_Basel_MOST.txt'
-    ViewFactorFileName = '_case7_ViewFactor_Vancouver_LCZ1.txt'
+    ViewFactorFileName = '_case5_BSPR_ViewFactor_Basel_MOST.txt'
+    # ViewFactorFileName = '_case7_ViewFactor_Vancouver_LCZ1.txt'
     # Case name to append output file names with
     # case = '_Ue2_BSPA_Only_VCWG_Basel_MOST'
     # case = '_CAPITOUL_MOST'
     # case = '_BSPR_Only_VCWG_Basel_MOST'
-    case = '_case7_VancouverRuralCorrectTimeOnlyVCWG_2008Jul'
+    case = 'Ue1_LiteratureAlbedo'
+    # case = '_case7_VancouverRuralCorrectTimeOnlyVCWG_2008Jul'
 
     # # Vancouver
     # # '''
@@ -47,19 +48,19 @@ if __name__ == '__main__':
     # case_name = 'CAPITOUL_2004_only_vcwg'
     # case_name = 'Vancouver_TopForcing_only_vcwg_2008_Jul'
     # case_name = 'Vancouver_Rural_only_vcwg_2008_Jul'
-    case_name = 'Vancouver_Rural_CorrectTime_only_vcwg_2008_Jul'
-    # case_name = 'BUBBLE_Ue1_Rural_only_vcwg_2002_June'
+    # case_name = 'Vancouver_Rural_CorrectTime_only_vcwg_2008_Jul'
+    case_name = 'BUBBLE_Ue1_only_vcwg_2002_June'
     # case_name = 'BUBBLE_Ue2_Rural_only_vcwg_2002_June'
     # saving_data_path = '_2_cases_input_outputs\\_07_Vancouver\\vcwg_saving'
     # saving_data_path = '_2_cases_input_outputs\\_08_CAPITOUL\\DOE_Ref_MediumOffice_4B\\vcwg_saving'
     # saving_data_path = '_2_cases_input_outputs\\_07_Vancouver\\TopForcing_Refined_SMALL_OFFICE\\vcwg_saving'
     # saving_data_path = '_2_cases_input_outputs\\_07_Vancouver\\Rural_Refined_Small_Office\\vcwg_saving'
-    saving_data_path = '_2_cases_input_outputs\\_07_Vancouver\\Rural_Refined_SmallOffice_4C_CorrectTime\\b_vcwg_saving'
-    # saving_data_path = '_2_cases_input_outputs\\_05_Basel_BSPR_ue1\\MidRiseApartment_4C_Rural\\vcwg_saving'
+    # saving_data_path = '_2_cases_input_outputs\\_07_Vancouver\\Rural_Refined_SmallOffice_4C_CorrectTime\\b_vcwg_saving'
+    saving_data_path = '_2_cases_input_outputs\\_05_Basel_BSPR_ue1\\MidRiseApart_4C_Rural_LiteratureAlbedo\\b_vcwg_saving'
     # saving_data_path = '_2_cases_input_outputs\\_06_Basel_BSPA_ue2\\Orientation_MidRiseApart_4C\\vcwg_saving'
+    start_time = '2002-06-10 00:00:00'
     # start_time = '2004-06-01 00:00:00'
-    start_time = '2008-07-01 00:00:00'
-    # start_time = '2002-06-10 00:00:00'
+    # start_time = '2008-07-01 00:00:00'
     time_interval_sec = 300
 
     vcwg_thread = Thread(target=run_vcwg)
