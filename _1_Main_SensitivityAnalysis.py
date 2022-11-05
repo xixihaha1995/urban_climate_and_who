2b15e537cbba3c2e01d8f899cb4950a098622476
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2022-11-04 18:13:43 -0600

    hard to debug the multiprocessing with pycharm

--- _1_Main_SensitivityAnalysis.py
+++ _1_Main_SensitivityAnalysis.py
@@ -20,6 +20,7 @@ if __name__ == '__main__':
     sensitivity_file = input("Please input the sensitivity file: "
                              "[SensitivityCAPITOUL_theta_canyon.ini]") or \
                        "SensitivityCAPITOUL_theta_canyon.ini"
+    # sensitivity_file = "SensitivityCAPITOUL_theta_canyon.ini"
     read_ini(sensitivity_file)
     uwgVariable = config['sensitivity']['uwgVariable']
     value_list = [float(i) for i in config['sensitivity']['value_list'].split(',')]
