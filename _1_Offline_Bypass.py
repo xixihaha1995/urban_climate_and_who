7649e88560ff657843c050f6163d34856fe53b64
Lichen <wulicheneason@gmail.com>
2022-11-09 08:05:38 -0700

    offline done

--- _1_Offline_Bypass.py
+++ _1_Offline_Bypass.py
@@ -27,10 +27,7 @@ def run_ep_api(input_value):
             idfFileName = coordination.config['_1_Main_Run_EP22_VCWG2.py']['idfFileName'][:-4] + '_Albedo0.7.idf'
     else:
         idfFileName = coordination.config['_1_Main_Run_EP22_VCWG2.py']['idfFileName']
-    # print(f'os.getpid(): {os.getpid()}, idfFileName: ', idfFileName)
-    epwFileName = coordination.config['_1_Main_Run_EP22_VCWG2.py']['epwFileName']
     state = coordination.ep_api.state_manager.new_state()
-    #coordination.ep_api.runtime.callback_end_system_timestep_after_hvac_reporting(state,time_step_handlers_1.smallOffice_get_ep_results)
     coordination.ep_api.exchange.request_variable(state, "HVAC System Total Heat Rejection Energy", "SIMHVAC")
     coordination.ep_api.exchange.request_variable(state, "Site Wind Speed", "ENVIRONMENT")
     coordination.ep_api.exchange.request_variable(state, "Site Wind Direction", "ENVIRONMENT")
@@ -48,7 +45,6 @@ def run_ep_api(input_value):
                                coordination.config['_0_vcwg_ep_coordination.py']['site_location'],
                                coordination.config['sensitivity']['theme'],
                                f'{coordination.uwgVariable}_{str_variable}ep_outputs')
-
     idfFilePath = os.path.join('./resources/idf', idfFileName)
     sys_args = '-d', output_path, '-w', coordination.generated_epw_path, idfFilePath
     coordination.ep_api.runtime.run_energyplus(state, sys_args)
