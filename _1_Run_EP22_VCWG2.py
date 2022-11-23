b666f353734168518098e5d939d3f6c59a47e3cc
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2022-11-22 23:11:22 -0700

    all the IDFs related shading should be ready

--- _1_Run_EP22_VCWG2.py
+++ _1_Run_EP22_VCWG2.py
@@ -37,7 +37,8 @@ def run_ep_api(input_config, experiments_theme, input_value):
 
     output_path = coordination.ep_trivial_path
     weather_file_path = os.path.join('.\\resources\\epw', epwFileName)
-    idfFilePath = os.path.join('.\\resources\\idf\\CAPITOUL_Possible_IDFs', idfFileName)
+    idfs_path = coordination.config['Default']['idfs_path']
+    idfFilePath = os.path.join(f'.\\resources\\idf\\{idfs_path}', idfFileName)
     sys_args = '-d', output_path, '-w', weather_file_path, idfFilePath
     coordination.ep_api.runtime.run_energyplus(state, sys_args)
 def info(title):
