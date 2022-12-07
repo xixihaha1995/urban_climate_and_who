8d87afd737838d3f20c23acdb91dfccf3385be64
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2022-12-06 21:29:28 -0700

    refactor the framework a little bit

--- VCWG_Hydrology.py
+++ VCWG_Hydrology.py
@@ -625,13 +625,11 @@ class VCWG_Hydro(object):
                 canTemp = numpy.mean(self.UCM.VerticalProfUrban.th[0:self.Geometry_m.nz_u])
                 canHum = numpy.mean(self.UCM.VerticalProfUrban.qn[0:self.Geometry_m.nz_u])
 
-                # self.BEM[i].building.BEMCalc(canTemp, canHum, self.BEM[i], MeteoData, ParCalculation, self.simTime,
-                #                              self.Geometry_m,
-                #                              self.FractionsRoof, self.EBCanyon.SWR, self.UCM.VerticalProfUrban,it)
-                # BEM, it, simTime, VerticalProfUrban, Geometry_m, MeteoData,
-                # FractionsRoof
-                self.BEM[i] = coordination.BEMCalc_Element(self.BEM[i],it, self.simTime,self.UCM.VerticalProfUrban,
-                                                           self.Geometry_m, MeteoData, self.FractionsRoof)
+                self.BEM[i].building.BEMCalc(canTemp, canHum, self.BEM[i], MeteoData, ParCalculation, self.simTime,
+                                             self.Geometry_m,
+                                             self.FractionsRoof, self.EBCanyon.SWR, self.UCM.VerticalProfUrban,it)
+                # self.BEM[i] = coordination.BEMCalc_Element(self.BEM[i],it, self.simTime,self.UCM.VerticalProfUrban,
+                #                                            self.Geometry_m, MeteoData, self.FractionsRoof)
                 ###
                 # Electricity consumption of urban area [W]
                 self.BEM[i].ElecTotal = self.BEM[i].building.ElecTotal * self.BEM[i].fl_area
@@ -640,12 +638,13 @@ class VCWG_Hydro(object):
                 # Mass
                 # self.BEM[i].mass.Element(0,0,0,0,self.TimeParam.dts,0.,1,self.BEM[i].building.fluxMass,self.BEM[i].building.fluxMass)
                 # # Roof
-                # if self.FractionsRoof.fimp > 0:
-                #     self.BEM[i].roofImp.Element(self.EBRoof.SWR.SWRabsRoofImp,self.EBRoof.LWR.LWRabsRoofImp,self.EBRoof.LEflux.LEfluxRoofImp,
-                #                                 self.EBRoof.Hflux.HfluxRoofImp,self.TimeParam.dts,0.,1,None,self.BEM[i].building.fluxRoof)
-                # if self.FractionsRoof.fveg > 0:
-                #     self.BEM[i].roofVeg.Element(self.EBRoof.SWR.SWRabsRoofVeg,self.EBRoof.LWR.LWRabsRoofVeg,self.EBRoof.LEflux.LEfluxRoofVeg,
-                #                                 self.EBRoof.Hflux.HfluxRoofVeg,self.TimeParam.dts,0.,1,None,self.BEM[i].building.fluxRoof)
+                if coordination.config['Bypass']['Over_Roof'] == "True":
+                    if self.FractionsRoof.fimp > 0:
+                        self.BEM[i].roofImp.Element(self.EBRoof.SWR.SWRabsRoofImp,self.EBRoof.LWR.LWRabsRoofImp,self.EBRoof.LEflux.LEfluxRoofImp,
+                                                    self.EBRoof.Hflux.HfluxRoofImp,self.TimeParam.dts,0.,1,None,self.BEM[i].building.fluxRoof)
+                    if self.FractionsRoof.fveg > 0:
+                        self.BEM[i].roofVeg.Element(self.EBRoof.SWR.SWRabsRoofVeg,self.EBRoof.LWR.LWRabsRoofVeg,self.EBRoof.LEflux.LEfluxRoofVeg,
+                                                    self.EBRoof.Hflux.HfluxRoofVeg,self.TimeParam.dts,0.,1,None,self.BEM[i].building.fluxRoof)
                 # # Walls
                 # self.BEM[i].wallSun.Element(self.EBCanyon.SWR.SWRabs.SWRabsWallSun,self.EBCanyon.LWR.LWRabs.LWRabsWallSun,
                 #                             self.EBCanyon.LEflux.LEfluxWallSun,self.EBCanyon.Hflux.HfluxWallSun,self.TimeParam.dts,
