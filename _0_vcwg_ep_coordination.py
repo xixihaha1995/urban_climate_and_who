e5b17caab28ac92d2185c1f85928d40062143e6b
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2022-12-11 19:37:19 -0700

    to compare the VCWG and all EP temperature

--- _0_vcwg_ep_coordination.py
+++ _0_vcwg_ep_coordination.py
@@ -1,8 +1,8 @@
 import configparser
 import threading, sys, os
 
-def ini_all(sensitivity_file_name):
-    global config, project_path, save_path_clean, sensor_heights,ep_trivial_path, data_saving_path, bld_type,\
+def ini_all(config_file_, value_):
+    global config, project_path, save_path_clean, sensor_heights,ep_trivial_path, data_saving_path,\
         ep_api, psychrometric,\
         vcwg_needed_time_idx_in_seconds, \
         vcwg_canTemp_K, vcwg_canSpecHum_Ratio, vcwg_canPress_Pa, vcwg_wsp_mps, vcwg_wdir_deg, \
@@ -12,18 +12,14 @@ def ini_all(sensitivity_file_name):
         mediumOfficeBld_footprint_area_m2, smallOfficeBld_footprint_area_m2,\
         footprint_area_m2
     # find the project path
-    config = configparser.ConfigParser()
     project_path = os.path.dirname(os.path.abspath(__file__))
-    config_path = os.path.join(project_path, '_0_configs', sensitivity_file_name)
-    config.read(config_path)
-    bld_type = config['Default']['bld_type']
+    config = config_file_
     experiments_theme = config['Default']['experiments_theme']
     save_path_clean = False
     sensor_heights = [float(i) for i in config['Default']['sensor_height_meter'].split(',')]
-    csv_file = config['Default']['csv_file']
     data_saving_path = os.path.join(project_path, 'A_prepost_processing',
-                                    experiments_theme,f'{csv_file}.csv')
-    ep_trivial_path = os.path.join(project_path, 'A_prepost_processing', experiments_theme, "ep_trivial_outputs")
+                                    experiments_theme,f'{value_}.csv')
+    ep_trivial_path = os.path.join(project_path, 'A_prepost_processing', experiments_theme, f"{value_}_ep_trivial_outputs")
     if config['Default']['operating_system'] == 'windows':
         sys.path.insert(0, 'C:/EnergyPlusV22-1-0')
     else:
@@ -39,19 +35,19 @@ def ini_all(sensitivity_file_name):
     vcwg_wsp_mps = 0
     vcwg_wdir_deg = 0
 
-    if "SmallOffice" in bld_type:
+    if "SmallOffice" in value_:
         footprint_area_m2 = 5500 * 0.09290304 / 1
-    elif "MediumOffice" in bld_type:
+    elif "MediumOffice" in value_:
         footprint_area_m2 = 53628 * 0.09290304 / 3
-    elif "LargeOffice" in bld_type:
+    elif "LargeOffice" in value_:
         footprint_area_m2 = 498588 * 0.09290304 / 12
-    elif "MidriseApartment" in bld_type:
+    elif "MidriseApartment" in value_:
         footprint_area_m2 = 33740 * 0.09290304 / 4
-    elif "StandAloneRetail" in bld_type:
+    elif "StandAloneRetail" in value_:
         footprint_area_m2 = 24962 * 0.09290304 /1
-    elif "StripMall" in bld_type:
+    elif "StripMall" in value_:
         footprint_area_m2 = 22500 * 0.09290304 / 1
-    elif "SuperMarket" in bld_type:
+    elif "SuperMarket" in value_:
         footprint_area_m2 = 45000 * 0.09290304 / 1
 
     ep_indoorTemp_C = 20
--- _1_Main_Run_EP22_VCWG2.py
+++ _1_Main_Run_EP22_VCWG2.py
@@ -1,18 +1,21 @@
+import configparser
 import os
+from multiprocessing import Process
 from threading import Thread
 import _0_vcwg_ep_coordination as coordination,\
     _1_ep_time_step_handlers_ver0 as time_step_handlers
 
-def run_ep_api():
+def run_ep_api(config,experiments_theme, value):
+    coordination.ini_all(config, value)
     state = coordination.ep_api.state_manager.new_state()
     coordination.psychrometric=coordination.ep_api.functional.psychrometrics(state)
-    if 'MediumOffice' in coordination.bld_type:
+    if 'MediumOffice' in value:
         coordination.ep_api.runtime.callback_end_system_timestep_after_hvac_reporting(state,
                                                                                       time_step_handlers.MediumOffice_get_ep_results)
-    elif 'SmallOffice' in coordination.bld_type:
+    elif 'SmallOffice' in value:
         coordination.ep_api.runtime.callback_end_system_timestep_after_hvac_reporting(state,
                                                                                       time_step_handlers.SmallOffice_get_ep_results)
-    elif 'LargeOffice' in coordination.bld_type:
+    elif 'LargeOffice' in value:
         coordination.ep_api.runtime.callback_end_system_timestep_after_hvac_reporting(state,
                                                                                       time_step_handlers.LargeOffice_get_ep_results)
     else:
@@ -25,16 +28,40 @@ def run_ep_api():
     coordination.ep_api.exchange.request_variable(state, "Site Outdoor Air Drybulb Temperature", "ENVIRONMENT")
     coordination.ep_api.exchange.request_variable(state, "Site Outdoor Air Humidity Ratio", "ENVIRONMENT")
 
-    idfFileName = coordination.config['Default']['idfFileName']
     epwFileName = coordination.config['Default']['epwFileName']
     output_path = coordination.ep_trivial_path
     weather_file_path = os.path.join('.\\resources\\epw', epwFileName)
-    idfFilePath = os.path.join(f'.\\resources\\idf', idfFileName)
+    idfFilePath = os.path.join(f'.\\resources\\idf\\IDF_Roof_Temperature', value+'.idf')
     sys_args = '-d', output_path, '-w', weather_file_path, idfFilePath
     coordination.ep_api.runtime.run_energyplus(state, sys_args)
+def read_ini(config_file_name):
+    global config
+    config = configparser.ConfigParser()
+    project_path = os.path.dirname(os.path.abspath(__file__))
+    config_path = os.path.join(project_path, '_0_configs',config_file_name)
+    config.read(config_path)
+def one_ini(sensitivity_file_name):
+    read_ini(sensitivity_file_name)
+    experiments_theme = config['Default']['experiments_theme']
+    value_list = [i for i in config['Default']['value_list'].split(',')]
+    this_ini_process = []
+    nbr_of_parallel = 5
+    batch_value_list = [value_list[i:i + nbr_of_parallel] for i in range(0, len(value_list), nbr_of_parallel)]
+    for batch_nbr, batch_value in enumerate(batch_value_list):
+        for value in batch_value:
+            this_ini_process.append(Process(target=run_ep_api, args=(config, experiments_theme, value)))
+        for process in this_ini_process:
+            process.start()
+        for process in this_ini_process:
+            process.join()
+        this_ini_process = []
+
+    # for value in value_list:
+    #     p = Process(target=run_ep_api, args=(config,experiments_theme, value))
+    #     p.start()
+    #     this_ini_process.append(p)
+
+
 
 if __name__ == '__main__':
-    coordination.ini_all('onlyEP_CAPITOUL.ini')
-    ep_thread = Thread(target=run_ep_api)
-    ep_thread.start()
-    ep_thread.join()
+    one_ini('CAPITOUL_Roof_Temperature.ini')
